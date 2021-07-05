6372ebe8cc050336910b2a891cfb5667276cc355
SOFe <sofe2038@gmail.com>
2021-07-05 13:26:54 +0800

    rustfmt

--- client/src/render/ui/mod.rs
+++ client/src/render/ui/mod.rs
@@ -13,7 +13,8 @@ pub struct Canvas {
 impl Canvas {
     /// Resets the canvas.
     pub fn reset(&self, dim: &Dimension) {
-        self.context.clear_rect(0., 0., dim.width.into(), dim.height.into());
+        self.context
+            .clear_rect(0., 0., dim.width.into(), dim.height.into());
     }
 }
 
