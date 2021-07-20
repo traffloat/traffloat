0d7bc2041563c0fd425984e06204d641c9493b18
SOFe <sofe2038@gmail.com>
2021-07-20 11:19:07 +0800

    Implemnted Shape::Cylinder without bb_under

--- common/src/shape.rs
+++ common/src/shape.rs
@@ -1,5 +1,7 @@
 //! Shape and appearance of an object
 
+use std::ops::RangeInclusive;
+
 use derive_new::new;
 use smallvec::{smallvec, SmallVec};
 use typed_builder::TypedBuilder;
@@ -46,6 +48,8 @@ pub enum Unit {
     Cube,
     /// A unit sphere `x^2 + y^2 + z^2 <= 1`
     Sphere,
+    /// A unit cylinder `x^2 + y^2 = 1, 0 <= z <= 1`
+    Cylinder,
 }
 
 impl Unit {
@@ -58,6 +62,7 @@ impl Unit {
                     && (-1. ..=1.).contains(&pos.z)
             }
             Self::Sphere => pos.x.powi(2) + pos.y.powi(2) + pos.z.powi(2) <= 1.,
+            Self::Cylinder => pos.x.powi(2) + pos.y.powi(2) <= 1. && (0. ..=1.).contains(&pos.z),
         }
     }
 
@@ -108,6 +113,41 @@ impl Unit {
                 let w = (Point::origin() - start).dot(&dir) / dir.norm_squared();
                 let closest = start + dir * w;
                 self.contains(closest).then(|| w)
+            }
+            Self::Cylinder => {
+                let delta = end - start;
+                let d2 = delta.norm_squared();
+                let discrim = d2 - (start.x * end.y - start.y * end.x).powi(2);
+
+                if discrim > 0. {
+                    let dr = discrim.sqrt();
+                    let base = start.x * delta.x + start.y * delta.y;
+                    let w1 = (-base - dr) / d2;
+                    let w2 = (-base + dr) / d2;
+
+                    let zw0 = -start.z / delta.z;
+                    let zw1 = (1. - start.z) / delta.z;
+
+                    fn intersect_ranges(
+                        a: RangeInclusive<f64>,
+                        b: RangeInclusive<f64>,
+                    ) -> Option<RangeInclusive<f64>> {
+                        if a.end() < b.start() || b.end() < a.start() {
+                            return None;
+                        }
+                        (a.end() >= b.start() && b.end() >= a.start()).then(|| {
+                            f64::max(*a.start(), *b.start())..=f64::min(*a.end(), *b.end())
+                        })
+                    }
+
+                    let range = intersect_ranges(
+                        intersect_ranges(0. ..=1., w1..=w2)?,
+                        f64::min(zw0, zw1)..=f64::max(zw0, zw1),
+                    )?;
+                    Some(*range.start())
+                } else {
+                    None
+                }
             }
         }
     }
@@ -183,6 +223,9 @@ impl Unit {
 
                 (min, max)
             }
+            Self::Cylinder => {
+                todo!()
+            }
         }
     }
 }
@@ -190,7 +233,7 @@ impl Unit {
 #[cfg(test)]
 mod tests {
     use super::Unit;
-    use crate::space::{Matrix, Vector};
+    use crate::space::{Matrix, Point, Vector};
 
     #[test]
     pub fn sphere_bb() {
@@ -239,6 +282,49 @@ mod tests {
                 (0., -2., 0.)..(2., 0., 2.)
             );
         }
+    }
+
+    #[test]
+    pub fn cylinder_between() {
+        macro_rules! assert_between {
+            (($x0:expr, $y0:expr, $z0:expr)..($x1:expr, $y1:expr, $z1:expr) => None) => {
+                let v0 = Point::new($x0, $y0, $z0);
+                let v1 = Point::new($x1, $y1, $z1);
+                let option = Unit::Cylinder.between(v0, v1);
+                if let Some(w) = option {
+                    panic!(
+                        "{}..{} should not intersect cylinder, got Some({})",
+                        v0, v1, w
+                    );
+                }
+            };
+            (($x0:expr, $y0:expr, $z0:expr)..($x1:expr, $y1:expr, $z1:expr) => Some($w:expr, $eps:expr)) => {
+                let v0 = Point::new($x0, $y0, $z0);
+                let v1 = Point::new($x1, $y1, $z1);
+                let option = Unit::Cylinder.between(v0, v1);
+                if let Some(w) = option {
+                    if ($w - w).abs() > $eps {
+                        panic!(
+                            "{}..{} should intersect cylinder at {} (\u{00b1} {}, got Some({})",
+                            v0, v1, $w, $eps, w
+                        );
+                    }
+                } else {
+                    panic!("{}..{} should intersect cylinder, got None", v0, v1);
+                }
+            };
+        }
+
+        assert_between!((-1., -1., 0.5)..(1., 1., 0.5) => Some((2f64.sqrt() - 1.) / (2f64.sqrt() * 2.), 1e-6));
+        assert_between!((-1., -1., -0.5)..(1., 1., -0.5) => None);
+        assert_between!((1., -1., 0.5)..(-1., 1., 0.5) => Some((2f64.sqrt() - 1.) / (2f64.sqrt() * 2.), 1e-6));
+        assert_between!((-1., 1., 0.5)..(1., -1., 0.5) => Some((2f64.sqrt() - 1.) / (2f64.sqrt() * 2.), 1e-6));
+        assert_between!((-2., 0., 0.5)..(0., 0., 0.5) => Some(0.5, 1e-6));
+        assert_between!((0., 0., 0.5)..(2., 0., 0.5) => Some(0., 1e-6));
+        assert_between!((0., 0., 1.5)..(1., 0., 0.5) => Some(0.5, 1e-6));
+        assert_between!((1., 0., 1.5)..(0., 0., 0.5) => Some(0.5, 1e-6));
+        assert_between!((0., 0., 0.5)..(1., 0., 1.5) => Some(0., 1e-6));
+        assert_between!((1., 0., 0.5)..(0., 0., 1.5) => Some(0., 1e-6));
     }
 }
 
